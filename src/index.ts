ac073f847cb1c5404b2953faf6ceba98f3e7b240
Mateusz Słomka <slomkam7@gmail.com>
2022-09-04 10:38:13 +0200

    create share and connect methods

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,17 @@
 import env from 'dotenv';
+import Cryptor, { SharerLink, SharerLinkContent } from './modules/sharerKeyCryptor';
 env.config();
 
+export const connect = ( link: SharerLink ) => {
+  const { ip, port } = Cryptor.decrypt(link);
+  console.log(`Connecting to: ${ip}:${port} ...`);
+}
 
-export {}
+
+export const share = ( content: SharerLinkContent ): SharerLink => {
+  const link = Cryptor.encrypt(content.ip, content.port);
+  console.log('Generated sharer link: ', link);
+  return link;
+}
+
+
